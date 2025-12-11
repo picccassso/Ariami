89a91e9854fdd2a809022196a7c753cbaeb61826
aienpme <139248352+aienpme@users.noreply.github.com>
2025-12-11 21:38:58 +0000

    CLI + Mobile fixes

--- bma_desktop/macos/Flutter/GeneratedPluginRegistrant.swift
+++ bma_desktop/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,7 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-import audioplayers_darwin
 import path_provider_foundation
 import screen_retriever_macos
 import shared_preferences_foundation
@@ -13,7 +12,6 @@ import tray_manager
 import window_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   ScreenRetrieverMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
