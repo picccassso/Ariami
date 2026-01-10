9c89939f2a1990a9c1a6053f78d3fe1024282ed5
aienpme <139248352+aienpme@users.noreply.github.com>
2026-01-10 20:25:42 +0000

    git action workflows update

--- ariami_cli/build-pi-release-mac.sh
+++ ariami_cli/build-pi-release-mac.sh
@@ -4,7 +4,7 @@ set -e
 # Ariami CLI - Raspberry Pi Release Builder for Mac
 # Builds ARM64 release using Docker (no Raspberry Pi needed)
 
-VERSION="1.0.1"
+VERSION="1.0.2"
 RELEASE_NAME="ariami-cli-raspberry-pi-arm64-v${VERSION}"
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
